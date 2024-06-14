c9d29179d2ceddd0c80283149e9deb0acb563ec3
sasi-selvaraj <sasiselvaraj1112@gmail.com>
2024-06-14 15:58:05 +0530

    Update webpack.mix.js new

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/assets/js/argon-dashboard.js')
+    .sass('resources/scss/argon-dashboard.scss', 'public/assets/css/argon-dashboard.css'
+          mix.js('resources/js/app.js', 'public/assets/js/argon-dashboard.js')
     .sass('resources/scss/argon-dashboard.scss', 'public/assets/css/argon-dashboard.css', [
         //
     ]);
